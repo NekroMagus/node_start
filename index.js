c0e5a3a70ad125bfdcbedab2d6f3cdd1292a8723
NekroMagus <NekroMagus@mail.ru>
2019-10-16 12:16:21 +0300

    add get methods

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.end('Travel');
+});
+
+app.get('/about', (req, res) => {
+
+});
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.end('404 - Страница не найдена');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Ошибка сервера');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express запущен на http://localhost:' + app.get('port'));
+});
